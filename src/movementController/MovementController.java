e59a5c4698cb783217b4364ccf0503b962afb07a
Otuslettia <95861205+Otuslettia@users.noreply.github.com>
2022-05-30 03:25:30 +0800

    Movement Controller Script Idea
    http://blog.csdn.net/qq_43235247/article/102861116

---
+++ src/movementController/MovementController.java
@@ -0,0 +1,13 @@
+package movementController;
+
+public class MovementController {
+	
+	private boolean AbleToControl = false;
+	public void StartControl() {
+		AbleToControl = true;
+	}
+	//BorderPane borderPane
+	private void onTick() {
+		
+	}
+}
--- src/scene/MapScene.java
+++ src/scene/MapScene.java
@@ -1,7 +1,6 @@
 package scene;
 
 import java.io.IOException;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -10,7 +9,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import player.NovicePlayer;
 //import javafx.scene.Node;
-import Main.RPG;
+//import Main.RPG;
 
 public class MapScene {
 	
@@ -18,12 +17,20 @@ public class MapScene {
     private Stage stage;
     private Scene scene;
     private Parent root;
-    
-    
+    private static int xLocation;
+    private static int yLocation;
+    //Q: When I call this scene, will the Location been RESET?
+    //Setting Scene
     NovicePlayer Player = NamingScene.CreateToMap();
     private void LocatePlayer() {
     	//read the kind of Player;
     	//put Player image in mid;
     }
+    private void SetMap() {
+    	//Set Array;
+    	//Read x and y Location
+    	//Array To Picture
+    }
+    //Use Movement script to control;
     
 }
